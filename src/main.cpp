fb968f200ae9f9b39afa09e632afff9cf7e28d28
maheshwariakshat34 <akshat.lakhotiya_cs.aiml24@gla.ac.in>
2026-02-11 09:49:50 +0530

    Add Addafruit SSD1306 AND GFX Library

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,29 @@
 #include <Arduino.h>
-void setup() {
-// write your initialization code here
+#include <Wire.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
+#define SCREEN_WIDTH 128
+#define SCREEN_HEIGHT 64
+#define OLED_ADDR 0x3C
+Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT);
+void setup()
+{
+    Serial.begin(9600);
+    if (!display.begin(SSD1306_SWITCHCAPVCC, OLED_ADDR))
+    {
+        Serial.println(F("SSD1306 allocation failed"));
+        while(true);
+    }
+    display.clearDisplay();
+    display.setTextSize(1);
+    display.setTextColor(SSD1306_WHITE);
+    display.setCursor(0, 0);
+    display.println("Arduino UNO R4");
+    display.println("OLED with I2C");
+    display.println("Hello, World!");
+    display.display();
 }
 
+
 void loop() {
-// write your code here
 }
